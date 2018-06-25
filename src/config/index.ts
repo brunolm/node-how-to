ad1e09cce0ee43cd43b467d8a1af3672718a773d
Bruno Leonardo Michels <brunolm7@gmail.com>
2018-06-24 22:01:36 -0300

    Add knexfile and config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,11 @@
+import * as dbConfig from './knexfile';
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+
 export default {
   api: {
     nasa: 'https://api.nasa.gov',
   },
+
+  db: dbConfig[nodeEnv],
 };
